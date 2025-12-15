3952408c514ac17b15432d7af0c6b372d5d3664d
basurtmart <mbd.97@hotmail.com>
2025-12-15 11:14:24 -0600

    chore: setup NativeWind and Tailwind CSS configuration
    - Added babel.config.js to configure Babel with NativeWind preset.
    - Created metro.config.js to integrate NativeWind with Expo's Metro bundler.
    - Added nativewind-env.d.ts for TypeScript support with NativeWind.
    - Configured tailwind.config.js with custom theme colors and font families.

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { Redirect } from 'expo-router';
+import React from 'react';
 
 const App = () => {
-  return (
-    <View>
-      <Text>¡Hola mundo!</Text>
-    </View>
-  )
-}
+  return <Redirect href="/home" />;
+  // return (
+  //   <SafeAreaView>
+  //     <View className='mt-6 mx-2.5'>
+  //       <Text className='text-5xl' style={{ fontFamily: 'WorkSans-Black' }}>
+  //         ¡Hola mundo!
+  //       </Text>
+
+  //       <Text className='text-4xl font-work-black text-primary'>
+  //         ¡Hola mundo!
+  //       </Text>
+
+  //       <Text className='text-3xl font-work-medium text-secondary'>
+  //         ¡Hola mundo!
+  //       </Text>
+
+  //       <Text className='text-2xl font-work-light text-secondary-100'>
+  //         ¡Hola mundo!
+  //       </Text>
+
+  //       <Text className='text-xl text-tertiary'>
+  //         ¡Hola mundo!
+  //       </Text>
+
+  //       <Link href="/products">
+  //         Productos
+  //       </Link>
+  //     </View>
+  //   </SafeAreaView>
+  // );
+};
 
 export default App
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.tsx",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./presentation/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#49129C',
+        secondary: {
+          DEFAULT: '#B40086',
+          100: '#C51297',
+          200: '#831266',
+        },
+        tertiary: '#EF2967',
+      },
+      fontFamily: {
+        'work-black': ['WorkSans-Black', 'sans-serif'],
+        'work-light': ['WorkSans-Light', 'sans-serif'],
+        'work-medium': ['WorkSans-Medium', 'sans-serif'],
+        'work-bold': ['WorkSans-Bold', 'sans-serif'],
+        'work-italic': ['WorkSans-Italic', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
